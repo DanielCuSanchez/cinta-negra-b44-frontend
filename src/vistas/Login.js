3cb4556c9b2a96532f598cbf40bc8076616c6c26
daniel cu <danielcu@alternet.com.mx>
2020-09-07 22:13:46 -0500

    login app

--- src/vistas/Login.js
+++ src/vistas/Login.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import { Button, Form, FormGroup, Input, Label } from 'reactstrap';
+import React, { useState } from 'react'
 
 export const Login = () => {
+    const [email, setEmail] = useState('')
+    const [password, setPasword] = useState('')
+    console.log('ESTO ES EL EMAIL: '+email)
+    console.log('ESTO ES EL PASSWORD: '+ password)
+    const handleForm = async (e) => {
+        e.preventDefault()
+        //Validar los datos
+        //Alphanum, detalles
+        const schemaCredentials = {email, password}
+        const URL_LOGIN = `${process.env.REACT_APP_BASE_BACKEND}/auth/login`
+        console.log({schemaCredentials,URL_LOGIN})
+
+        try {
+            const respuesta = await fetch(URL_LOGIN,{method:'POST', type:'json', mode: 'cors' , body: schemaCredentials })
+            const res = await respuesta.json()
+            alert('Login sucess')
+            console.log(res)
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
     return (
-        <div>
-            <h1>Login</h1>
-        </div>
+        <>
+            <h1>Login Tareas</h1>
+            <Form onSubmit={handleForm}>
+                <FormGroup>
+                    <Label for="exampleEmail">Email</Label>
+                    <Input type="email" value={email} onChange={(e)=>{ setEmail(e.target.value)}} name="email" id="exampleEmail" placeholder="Ingresa tu email" />
+                </FormGroup>
+                <FormGroup>
+                    <Label for="examplePassword">Password</Label>
+                    <Input type="password" value={password}  onChange={(e)=>{ setPasword(e.target.value)}} name="password" id="examplePassword" placeholder="Ingresa tu password" />
+                </FormGroup>
+                <Button>Iniciar sesión</Button>
+            </Form>
+        </>
     )
 }
--- src/vistas/Signup.js
+++ src/vistas/Signup.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 export const Signup = () => {
+    document.title = 'Signup'
     return (
         <div>
             <h1>Signup</h1>
